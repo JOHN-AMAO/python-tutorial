e662e1bf661c2067663941cc0e6203af69bb06ea
JOHN-AMAO <amaojohn767@gmail.com>
2024-01-02 02:30:36 +0100

    done

---
+++ day 2/conditionals.py
@@ -0,0 +1,11 @@
+def inspect(x):
+    if x == 0:
+        print("X is Zero")
+    elif x == 1:
+        print("X is one")
+    elif x == 2:
+        print("X is two")
+    else:
+        print("X is greater than 2")
+
+inspect(18)
