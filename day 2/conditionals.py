f0ca8350d49dbccb0c93446eb0ff9801a0daa441
JOHN-AMAO <amaojohn767@gmail.com>
2024-01-02 23:43:39 +0100

    conditionals

--- day 2/conditionals.py
+++ day 2/conditionals.py
@@ -8,4 +8,10 @@ def inspect(x):
     else:
         print("X is greater than 2")
 
-inspect(18)
+
+def to_smash(total_candies):
+    print("splitting", total_candies, "candy" if total_candies == 1 else "candies" )
+    return total_candies % 3
+
+to_smash(93)
+to_smash(1)
