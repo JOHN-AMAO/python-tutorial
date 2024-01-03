cb11839848f40fccbaf2fd46e75f1f01522f8ca8
JOHN-AMAO <amaojohn767@gmail.com>
2024-01-03 03:49:02 +0100

    loops

---
+++ day 3/loops.py
@@ -0,0 +1,13 @@
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+
+for char in s:
+    if char.isupper():
+        print(char, end=" ")
+
+for i in range(5):
+    print("Doing important work, i =", i)
+
+i = 0
+while i < 10:
+    print("i is ", i)
+    i += 1
